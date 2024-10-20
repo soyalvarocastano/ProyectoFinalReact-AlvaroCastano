13b8700516ae726cf8b0a0cf3acb6c0ee0aacfd8
alvaro luis castaño <alvarocastano_@outlook.com>
2024-10-20 23:12:04 +0200

    agregar funcion a boton

--- src/views/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/views/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,17 +1,20 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { useParams } from "react-router-dom"
 import { getSingleProduct } from "../../firebase/firebase.js";
 import Footer from "../../componets/Footer.jsx"
 import LoadingComponent from "../../componets/LoadingComponent.jsx";
+import { CartContext } from "../../context/CartContext.jsx"; 
+import Swal from 'sweetalert2';
 
 export default function ItemDetailContainer(){
   
     const [product, setProduct]= useState({})
-  const [isLoading, setIsLoading] = useState(true)
+    const [isLoading, setIsLoading] = useState(true)
     const {id} = useParams();
+    const [,, addItem] = useContext(CartContext)
 
     useEffect(() => {
-      setIsLoading(true); // Inicia la carga
+      setIsLoading(true); 
       getSingleProduct(id).then((productData) => {
         setProduct(productData);
         setTimeout(() => {
@@ -19,6 +22,17 @@ export default function ItemDetailContainer(){
         }, 1000);
       });
     }, [id]);
+
+    const handleAddToCart = () => {
+      addItem(product);
+      Swal.fire({
+        icon: 'success',
+        title: 'Producto añadido al carrito',
+        text: `${product.name} ha sido añadido al carrito.`,
+        timer: 2000,
+        showConfirmButton: false
+      });
+    };
   
     
     if (isLoading) {
@@ -45,7 +59,7 @@ export default function ItemDetailContainer(){
           <h2 className="text-3xl font-bold mb-4">{product.name}</h2>
           <p className="text-lg mb-4 text-gray-300">{product.description}</p>
           <p className="text-2xl font-bold text-red-500">{product.price} €</p>
-          <button className="bg-slate-50 hover:bg-red-500 text-black font-bold py-2 px-4 rounded-full transition duration-300 ease-in-out" >
+          <button className="bg-slate-50 hover:bg-red-500 text-black font-bold py-2 px-4 rounded-full transition duration-300 ease-in-out" onClick={handleAddToCart} >
             Agregar
           </button>
         </div>
